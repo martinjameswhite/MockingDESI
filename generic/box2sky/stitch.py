c283885c9fd942aca7f5014f4dbb33c069ad89e2
Martin White <mjwhite@lbl.gov>
2015-12-20 13:27:40 -0800

    Initial commit: stub.

---
+++ generic/box2sky/stitch.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+#
+# Generates a number of different projections of periodic boxes onto the
+# sky, each covering a different redshift, using box2sky.
+# Stitches these together to get a single "light cone" output.
+# Uses interpolation of the positions and velocities to the
+# appropriate redshift.
+#
+from __future__ import print_function,division
+
+
+import numpy  as np
+import ndfilehandler as FH
+import subprocess
+import sys
+
+
+__author__ = "Martin White"
+__version__ = "1.0"
+__email__  = "mjwhite@lbl.gov"
+
+
+
+def stitch():
+    """    
+    Does the work.
+    """
+    print("This is currently a stub.")
+    #
+
+
+
+if __name__=="__main__":
+    if len(sys.argv)!=1:
+        raise RuntimeError,\
+          "Usage: %s"%sys.argv[0]
+    else:
+      stitch()
+    #
