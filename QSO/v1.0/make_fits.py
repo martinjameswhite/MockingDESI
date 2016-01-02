7560548bdfbe692fb95826d7a75e3c59d8c22160
Martin White <mjwhite@lbl.gov>
2016-01-01 17:41:51 -0800

    Initial commit.

---
+++ QSO/v1.0/make_fits.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+#
+# Takes the results produced by the upstream codes and packages
+# them into "final" FITS files which can also be moved to an
+# appropriate location.
+#
+# Uses Erin Sheldon's FITSIO package:
+#	https://github.com/esheldon/fitsio
+# to read the FITS file.
+#
+from __future__ import print_function,division
+
+
+import numpy  as np
+import fitsio as F
+import ndfilehandler as FH
+import sys
+
+
+# This is currently a stub.
+
+
+if __name__=="__main__":
+    pass
+    #
